a0fb357498ede7e020959d379216b834ab52ed63
Nesrullah Hacizade <nesrullah.hacizade@gmail.com>
2022-03-17 23:42:04 +0400

    feature user

---
+++ User/Models/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using User;
+namespace User
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user1 = new User();
+            user1.Username = "Nicat";
+            user1.Password = "Hecinecesenficuu";
+            user1.Age = -5;
+            user1.GetInfo();
+            //Console.WriteLine();
+            
+            
+        }
+    }
+}
---
+++ User/Models/user.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace User
+{
+    class User
+    {
+        private string username;
+        private string password;
+        private int age ;
+        public string Username { get;set; }
+        public string Password { get;set; }
+        public int Age 
+        { 
+            get 
+            {
+                return age;
+            } 
+            set 
+            {
+                if (value > 0)
+                    age = value;
+
+                else
+                    Console.WriteLine("yas menfi olmaz");
+            }
+        }      
+        public User()
+        {
+
+        }
+        public User(string username, string password, int age)
+        {
+            this.Username = username;
+            this.Password = password;
+            this.Age = age;
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine($"{Username}\n{Password}\n{Age}");
+        }
+
+
+    }
+}
